9f6a618e0ebe49d3e0eb0430eb64489e8e49cb56
aperezdom <aideperz@gmail.com>
2021-03-20 20:01:08 -0600

    Getting the basics part 2a

---
+++ js/script.js
@@ -0,0 +1,55 @@
+
+
+// NODE ELEMENTS //
+
+const startButton = document.querySelector("button");
+const dreamButton = document.querySelector("button");
+const nightmareButton = document.querySelector("button");
+const giveUpButton = document.querySelector("button");
+
+// GLOBAL VARIABLES //
+
+// CLASSES //
+
+class userPlayer{
+    constructor(name){
+        this.name = name;
+        this.playerHealth = 50;
+        this.dreamPower = 10;
+        this.nightmarePower = 5;
+    }
+
+
+    dreamAttack(){
+        console.log("Dream attack!");
+    }
+    
+    nightmarePower(){
+        console.log("Nightmare attack!");
+    }
+
+}
+
+
+
+class compEnemy {
+    constructor(name){
+        this.name = name;
+        this.enemyHealth = enemyHealth;
+        this.enemyPower = enemyPower;
+        
+    }
+
+    enemyAttack(){
+        console.log("Enemy attack!");
+    }
+}
+
+const player = new userPlayer ("Alex");
+
+// FUNCTIONS //
+
+// EVENT LISTENERS //
+
+// console.log("uno, dos, tres, si, si");
+// console.log("Te quiero mucho");
