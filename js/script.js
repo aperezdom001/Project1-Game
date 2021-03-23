6ad0826d1610ca1db5d6f62d8b923c3216f02028
aperezdom <aideperz@gmail.com>
2021-03-22 22:20:58 -0600

    Functions

--- js/script.js
+++ js/script.js
@@ -9,6 +9,8 @@ const giveUpButton = document.querySelector("button");
 
 // GLOBAL VARIABLES //
 
+let score = 0;
+
 // CLASSES //
 
 class userPlayer{
@@ -22,7 +24,7 @@ class userPlayer{
 
     dreamAttack(enemy){
         console.log("Dream attack!");
-        enemy.health = dreamPower - enemy.health;
+        enemy.health = this.dreamPower - enemy.health;
         console.log(enemy.health);
     }
     
@@ -39,12 +41,12 @@ class userPlayer{
 class compEnemy {
     constructor(name){
         this.name = name;
-        this.enemyHealth = enemyHealth;
-        this.enemyPower = enemyPower;
+        this.enemyHealth = 15;
+        this.enemyPower = 3;
         
     }
-
-    enemyAttack(player){
+ 
+    enemyAttack(player){  //for now, add the type of orbs later 
         console.log("Enemy attack!");
         player.health = this.enemyPower - player.health;
         console.log(player.health);
@@ -52,8 +54,33 @@ class compEnemy {
 }
 
 const player1 = new userPlayer ("Alex");
+const enemy1 = new compEnemy("Enemy 1");
+
+console.log(player1);
+console.log(enemy1);
+
+player1.dreamAttack(enemy1);
+console.log(player1.dreamAttack(enemy1));
 
 // FUNCTIONS //
+
+const startGame = () => {
+    console.log("Start!!");
+}
+
+const scoreUpdate = () => {
+    for(let i = 0; i < 0; player1.playerHealth++){
+        return player1.playerHealth;
+       
+    }
+    console.log("SCORE: " + score);
+}
+
+const gameOver = () => {
+    console.log("Whoops, game over.");
+
+}
+
 
 // EVENT LISTENERS //
 
