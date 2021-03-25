0a84502f3752442a85742e33fbdb3f00576a1223
aperezdom <aideperz@gmail.com>
2021-03-25 14:58:49 -0600

    Give up, rounds, and health changes made

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ const nightmareButton = document.querySelector("#nightmare");
 const giveUpButton = document.querySelector("#give-up");
 const scoreBoard = document.querySelector("#score");
 const threeRounds = document.querySelector("#firstRound");
+const pHealth = document.querySelector("#health");
 
 // GLOBAL VARIABLES //
 
@@ -45,7 +46,9 @@ class compEnemy {
     constructor(name){
         this.name = name;
         this.enemyHealth = 25;
-        this.enemyPower = this.enemyPower;
+        this.enemyPower = Math.floor(Math.random() * 3 + 4);
+        
+
         
     }
  
@@ -57,8 +60,8 @@ class compEnemy {
 }
 
 const player1 = new userPlayer ("Alex");
-const enemy1 = new compEnemy("Enemy 1", 3);
-const enemy2 = new compEnemy("Enemy 2", 6);
+const enemy1 = new compEnemy("Enemy 1");
+const enemy2 = new compEnemy("Enemy 2");
 
 
 // console.log(player1);
@@ -81,22 +84,33 @@ const startGame = () => {
 
 //notes: reach a certain amount in your score to go to the next round
 const theRounds = (nextRound = 1) => {
-    while(score >= 5){
-        rounds =+ nextRound;
-        threeRounds.innerHTML =score;
-        //Round two begins
-       // console.log("Round2");
-       if(score >=15){
-           //Round three
-           rounds =+ nextRound;
-           threeRounds.innerHTML =score;
-       }
-       else if (score>=25){
-           //round four!
-           rounds =+ nextRound;
-           threeRounds.innerHTML =score;
-       }
+    if(score >= 25){
+        rounds = 4;
+        
+    }else if(score >= 15){
+        rounds = 3;
+    }else if (score >= 5){
+        rounds = 2;
     }
+
+    threeRounds.innerHTML = rounds;
+
+    // if(score >= 5){
+    //     rounds =+ nextRound;
+    //     threeRounds.innerHTML =score;
+    //     //Round two begins
+    //    // console.log("Round2");
+    //    if(score >=15){
+    //        //Round three
+    //        rounds =+ nextRound;
+    //        threeRounds.innerHTML =score;
+    //    }
+    //    else if (score>=25){
+    //        //round four!
+    //        rounds =+ nextRound;
+    //        threeRounds.innerHTML =score;
+    //    }
+    // }
 }
 
 const scoreUpdate = (points = 0) => { 
@@ -108,15 +122,17 @@ const scoreUpdate = (points = 0) => {
 const giveUp = () => { //ask questionxs
     const giveAlert = prompt("Your health is " +
     player1.playerHealth + ". Do you want to give up? yes or no?");
-    if(player1.playerHealth <= 15){
-        giveAlert;
-    } else if (giveAlert === "yes"){
+    
+     if (giveAlert === "yes"){
         const yesPrompt = alert ("Oh no! You became an evil creature and will forever haunt those tho run into you...");
         yesPrompt;
+        window.location.reload();
     }else if (giveAlert === "no"){
         const noPrompt = alert ("Keep fighting!!! Don't lose hope.");
         noPrompt;
     }
+
+  
 }
 
 const gameOver = () => {
@@ -134,17 +150,21 @@ startButton.addEventListener("click", startGame);
 dreamButton.addEventListener("click", () =>{
     player1.dreamAttack(enemy1);
     scoreUpdate(5);
+    theRounds();
     enemy1.enemyAttack(player1);
+    pHealth.innerHTML = player1.playerHealth;
 });
 nightmareButton.addEventListener("click", () => {
     player1.nightmareAttack(enemy1);
     player1.nightmareAttack(enemy2);
+    theRounds();
     scoreUpdate(8);
     enemy1.enemyAttack(player1);
     enemy2.enemyAttack(player1);
+    pHealth.innerHTML = player1.playerHealth;
 });
 giveUpButton.addEventListener("click", () => {
-    scoreUpdate(0);
+    giveUp(); 
 });
 
 
